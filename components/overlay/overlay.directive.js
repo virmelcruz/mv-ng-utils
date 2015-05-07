912b396be5700e7a44fb5513c810ebcb7626eb7b
ThisIsMarkSantiago <mesantiago@dynamicobjx.com>
2015-05-07 13:29:27 +0800

    Overlay component

---
+++ components/overlay/overlay.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('afaApp')
+.directive('overlay', function() {
+	return {
+		link: function(scope, element, attrs){
+			element.attr('class','overlay');
+			var head = element.attr('head') ? element.attr('head') :
+						element.data('head') ? element.data('head') :
+						false;
+			var foot = element.attr('foot') ? element.attr('foot') :
+						element.data('foot') ? element.data('foot') :
+						false;
+			if(head){
+				element.prepend("<div class='head'>"+ head +"</div>");
+			}
+			if(foot){
+				element.append("<div class='foot'>"+ foot +"</div>");
+			}
+			element.children().wrapAll("<div class='container'><div class='overlay-box'></div></div>");
+		}
+	}
+});
